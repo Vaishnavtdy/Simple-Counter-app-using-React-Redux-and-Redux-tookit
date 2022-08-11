a268a80e95ba174319b1f25bfa9b8cd2f324a02a
Vaishnavtdy <vaishnavtdy@gmail.com>
2022-08-11 12:04:43 +0530

    second commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment, decrement, resetVal } from '../features/counterSlice'
+import { increment, decrement, resetVal } from '../slices/counterSlice'
 import './Counter.css'
 
 function Counter() {
